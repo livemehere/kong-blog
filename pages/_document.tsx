51559876b49cd389edde28e749a5bb1e75337aba
livemehere <rhdxoals@gmail.com>
2023-07-23 23:37:59 +0900

    ✨ gTag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GTDJQ4G9SN"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-GTDJQ4G9SN');
+          </script>
+        `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
