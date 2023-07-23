6369d12a615ee825d3d2722aeb3789233a37068d
livemehere <rhdxoals@gmail.com>
2023-07-23 23:41:59 +0900

    ✨ ga4 프로덕션에서만 동작하도록 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,32 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { IS_PROD } from '@src/config';
 
 /* 공통으로 SSR 되는 html 마크업 */
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GTDJQ4G9SN"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        {IS_PROD && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-GTDJQ4G9SN"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
           <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-          
+
             gtag('config', 'G-GTDJQ4G9SN');
           </script>
         `,
-          }}
-        />
+              }}
+            />
+          </>
+        )}
       </Head>
 
       <body>
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
 export const DOMAIN = process.env.DOMAIN || 'localhost';
+export const IS_PROD = process.env.NODE_ENV === 'production';
