11fa92a3906745f7c08ffa108b32152e896ef890
livemehere <rhdxoals@gmail.com>
2023-08-16 23:02:18 +0900

    fix: mobile user scale disable

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         {IS_PROD && (
           <>
             <script
